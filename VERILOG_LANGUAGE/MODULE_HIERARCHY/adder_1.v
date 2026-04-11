8daef8d5031d7590ace17c6b6ab8059670d85f2d
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-04-11 18:09:37 +0530

    Create adder_1.v

---
+++ VERILOG_LANGUAGE/MODULE_HIERARCHY/adder_1.v
@@ -0,0 +1,10 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire cin,cout;
+    add16 a1(.a(a[15:0]),.b(b[15:0]),.cin(0),.sum(sum[15:0]),.cout(cout));
+    add16 a2(.a(a[31:16]),.b(b[31:16]),.cin(cout),.sum(sum[31:16]));       
+
+endmodule
