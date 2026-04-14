c40267ab3916b507d957cac1dd59bdaff793c5d4
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-04-14 12:13:21 +0530

    Create 2_to_1-bus_multiplexer.v

---
+++ CIRCUITS/COMBINATIONAL_LOGIC/MULTIPLEXERS/2_to_1-bus_multiplexer.v
@@ -0,0 +1,13 @@
+module top_module( 
+    input [99:0] a, b,
+    input sel,
+    output [99:0] out );
+    always@(*)begin
+        if(sel==0)
+            out = a;
+        else
+            out = b;
+    end
+           
+
+endmodule
