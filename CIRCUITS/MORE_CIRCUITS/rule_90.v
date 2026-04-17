212e97d421af52b4d69586c193bf254e043e7c8a
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-04-17 12:46:00 +0530

    Create rule_90.v

---
+++ CIRCUITS/MORE_CIRCUITS/rule_90.v
@@ -0,0 +1,14 @@
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q );
+    always@(posedge clk)begin
+        if(load)
+            q<=data;
+        else
+            q<= q<<1 ^ q>>1;
+    end
+        
+
+endmodule
