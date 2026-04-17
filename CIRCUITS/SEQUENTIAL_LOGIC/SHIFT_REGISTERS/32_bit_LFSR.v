4a148e39210bbd3847fa460ef6e0398c19b20581
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-04-17 12:01:18 +0530

    Create 32_bit_LFSR.v

---
+++ CIRCUITS/SEQUENTIAL_LOGIC/SHIFT_REGISTERS/32_bit_LFSR.v
@@ -0,0 +1,19 @@
+module top_module(
+    input clk,
+    input reset,    // Active-high synchronous reset to 32'h1
+    output [31:0] q
+); 
+    
+    always@(posedge clk) begin
+        if(reset)
+            q <= 32'h1;
+        else begin
+        	q[30:0] <= q[31:1];
+        	q[31] <= q[0]^0;
+        	q[21] <= q[0]^q[22];
+        	q[1] <= q[0]^q[2];
+        	q[0] <= q[0]^q[1];
+        end
+    end
+        
+endmodule
