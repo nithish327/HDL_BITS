a298036483825abe961f62ccc0bce7c75594df7a
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-04-30 13:03:23 +0530

    Create simple_fsm_3(synchronous_reset).v

---
+++ CIRCUITS/FINITE_STATE_MACHINE/simple_fsm_3(synchronous_reset).v
@@ -0,0 +1,54 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,
+    output out); //
+    reg [1:0]state,next_state;
+    parameter A=0,B=1,C=2,D=3;
+    
+
+    // State transition logic
+    always@(*)begin
+        case(state)
+            A : begin
+                if(in)
+                    next_state <= B;
+                else
+                    next_state <= A;
+            end
+            B : begin
+                if(in)
+                    next_state <= B;
+                else
+                    next_state <= C;
+            end
+            C : begin
+                if(in)
+                    next_state <= D;
+                else
+                    next_state <= A;
+            end
+            D : begin
+                if(in)
+                    next_state <= B;
+                else
+                    next_state <= C;
+            end
+        endcase
+    end
+            
+            
+
+    // State flip-flops with synchronous reset
+    always@(posedge clk)begin
+        if(reset)
+            state <= 0;
+        else
+            state <= next_state;
+    end
+    
+
+    // Output logic
+    assign out = (state == D);
+
+endmodule
