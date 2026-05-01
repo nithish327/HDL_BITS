7ebabb9fe378d270f8a454f53cec87e419c62538
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-05-01 13:04:36 +0530

    Create and_gate.v

---
+++ VERIFICATION : WRITING_TESTBENCHES/and_gate.v
@@ -0,0 +1,12 @@
+module top_module();
+    reg [1:0]in;
+    wire out;
+    andgate dut (.in(in),.out(out));
+    initial begin
+        in = 2'b00;#10;
+        in = 2'b01;#10;
+        in = 2'b10;#10;
+        in = 2'b11;#10;
+    end
+ 
+endmodule
