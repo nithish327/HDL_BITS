b8e490699d182e60b6b4d434c0946403d00e8538
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-05-01 13:05:39 +0530

    Add testbench for top_module in testbench_2.v

---
+++ VERIFICATION : WRITING_TESTBENCHES/testbench_2.v
@@ -0,0 +1,18 @@
+module top_module();
+    reg clk;
+    reg in;
+    reg[2:0]s;
+    wire out;
+    q7 dut(.clk(clk),.in(in),.s(s),.out(out));
+    always #5 clk= ~clk;
+    initial begin
+        clk=0;
+        in =0; s=3'd2;#10; 
+        in =0; s=3'd6;#10; 
+        in =1; s=3'd2;#10; 
+        in =0; s=3'd7;#10; 
+        in =1; s=3'd0;#30;
+        in =0; s=3'd0;#10;
+    end
+
+endmodule
