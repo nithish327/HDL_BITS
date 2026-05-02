20a729fae227b5ae8d17f498ca4487dbba9b4a5b
Nithish S <146941287+nithish327@users.noreply.github.com>
2026-05-02 15:24:38 +0530

    Implement finite state machine in Verilog

---
+++ CIRCUITS/FINITE_STATE_MACHINE/Q2a:fsm .v
@@ -0,0 +1,52 @@
+module top_module (
+    input clk,
+    input resetn,    // active-low synchronous reset
+    input [3:1] r,   // request
+    output [3:1] g   // grant
+); 
+    parameter A=0,B=1,C=2,D=3;
+    reg [1:0]state,next_state;
+    always @(posedge clk) begin
+        if(!resetn)
+            state<=A;
+        else
+            state<=next_state;
+    end
+    always @(*)begin
+        case(state)
+            A: begin
+                if(r[1])
+                    next_state=B;
+                else if(r[2])
+                    next_state=C;
+                else if(r[3])
+                    next_state=D;
+                else
+                    next_state=A;
+            end
+            B: begin
+                if(r[1])
+                    next_state=B;
+                else
+                    next_state=A;
+            end
+            C: begin
+                if(r[2])
+                    next_state=C;
+                else
+                    next_state=A;
+            end
+            D: begin
+                if(r[3])
+                    next_state=D;
+                else
+                    next_state=A;
+            end
+            default:next_state=A;
+        endcase
+    end
+    assign g[1]=(state==B);
+    assign g[2]=(state==C);
+    assign g[3]=(state==D);
+
+endmodule
